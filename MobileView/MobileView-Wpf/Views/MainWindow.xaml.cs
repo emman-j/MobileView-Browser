bce3c75f192778c1b0b878b5e773105f41b61107
j-emman <176351546+emman-j@users.noreply.github.com>
2025-05-26 15:48:51 +0800

    updated main window

--- MobileView/MobileView-Wpf/Views/MainWindow.xaml.cs
+++ MobileView/MobileView-Wpf/Views/MainWindow.xaml.cs
@@ -10,10 +10,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MobileView_Wpf
+namespace MobileView_Wpf.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
