244be49a19ee6741f1a83719509cb87865cdd0f4
j-emman <176351546+emman-j@users.noreply.github.com>
2025-05-26 15:49:05 +0800

    added behavior class for window drag

---
+++ MobileView/MobileView-Wpf/Infrastructure/Behavoirs/WindowDrag.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows;
+
+namespace MobileView_Wpf.Infrastructure.Behaviors
+{
+    public static class WindowDrag
+    {
+        [DllImport("user32.dll")]
+        private static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int msg, int wParam, int lParam);
+
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int HTCAPTION = 0x2;
+
+        public static readonly DependencyProperty IsDragEnabledProperty =
+            DependencyProperty.RegisterAttached(
+                "IsDragEnabled",
+                typeof(bool),
+                typeof(WindowDrag),
+                new UIPropertyMetadata(false, OnIsDragEnabledChanged));
+
+        public static void SetIsDragEnabled(UIElement element, bool value)
+        {
+            element.SetValue(IsDragEnabledProperty, value);
+        }
+
+        public static bool GetIsDragEnabled(UIElement element)
+        {
+            return (bool)element.GetValue(IsDragEnabledProperty);
+        }
+
+        private static void OnIsDragEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is UIElement element)
+            {
+                if ((bool)e.NewValue)
+                {
+                    element.MouseLeftButtonDown += Element_MouseLeftButtonDown;
+                }
+                else
+                {
+                    element.MouseLeftButtonDown -= Element_MouseLeftButtonDown;
+                }
+            }
+        }
+
+        private static void Element_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is DependencyObject depObj)
+            {
+                var window = Window.GetWindow(depObj);
+                if (window != null)
+                {
+                    ReleaseCapture();
+                    SendMessage(new WindowInteropHelper(window).Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+                }
+            }
+        }
+    }
+}
