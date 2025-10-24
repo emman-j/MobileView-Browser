bbd7124ffc340989e649b28711eaed806ef97c65
emman-j <176351546+emman-j@users.noreply.github.com>
2025-10-24 12:48:39 +0800

    added wv2 interface

---
+++ MobileView/MobileView.Core/IWV2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileView.Core
+{
+    public interface IWV2
+    {
+        string SiteTitle { get; set; }
+        string ProfileName { get; set; }
+        string URL { get; set; }
+        string ProfileFolder { get; set; }
+        string UserAgent { get; set; }
+        List<string> ExtensionsPath { get; set; }
+        CoreWebView2Environment Environment { get; set; }
+        CoreWebView2Profile Profile { get; set; }
+        WebViewWrapper WebControl { get; set; }
+    }
+}
