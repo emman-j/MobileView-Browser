162ce7fcafc2808a292a96f1960f49a56f12eaa0
emman-j <176351546+emman-j@users.noreply.github.com>
2025-10-24 13:02:43 +0800

    added winforms project to test core lib

---
+++ MobileView/MobileView.Winforms/Forms/Form_HistoryManager.cs
@@ -0,0 +1,61 @@
+﻿using MobileView.Core;
+using MobileView.Winforms.Utilities;
+using System.Data;
+
+namespace MobileView.Winforms
+{
+    public partial class Form_HistoryManager : Form
+    {
+        private readonly WV2Service Browser;
+        private readonly FormManager formManager;
+        private DataTable HistoryTable;
+        public Form_HistoryManager(WV2Service _webViewService, Form? currentform = null)
+        {
+            InitializeComponent();
+            formManager = new FormManager(this);
+            Browser = _webViewService;
+            formManager.PreserveCurrentFormLocationAndSize(currentform);
+        }
+        private async void BindDataGridView(DataGridView datagridview)
+        {
+            datagridview.DataSource = null;
+            datagridview.Rows.Clear();
+            datagridview.Columns.Clear();
+            HistoryTable = await Browser.GetHistory();
+            datagridview.DataSource = HistoryTable;
+            datagridview.Columns["Visit Count"].Visible = false;
+            datagridview.Columns["URL"].Visible = false;
+            datagridview.Columns["ID"].Visible = false;
+        }
+        private void Form_HistoryManager_Shown(object sender, EventArgs e)
+        {
+            BindDataGridView(dataGridView1);
+        }
+        private void ReloadButton_Click(object sender, EventArgs e)
+        {
+            BindDataGridView(dataGridView1);
+        }
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridView datagridview = (DataGridView)sender;
+            DataGridViewRow row = datagridview.Rows[e.RowIndex];
+
+            string? URL = row.Cells["URL"].Value.ToString();
+            if (string.IsNullOrWhiteSpace(URL)) { return; }
+            Browser.Navigation.GoTo(URL);
+            this.Close();
+        }
+        private void MenuButton_Click(object sender, EventArgs e)
+        {
+            MenuPanel.Visible = !MenuPanel.Visible;
+        }
+        private void clearAllBrowsingHistoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Browser.Clear.AllBrowsingData();
+        }
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
