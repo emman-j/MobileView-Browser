41cfc9ea51d9aeeb6a1af5b149eb7ee5106a571d
emman-j <176351546+emman-j@users.noreply.github.com>
2025-11-14 13:54:18 +0800

    created a settings manager

---
+++ MobileView/MobileView.Winforms/Utilities/SettingsManager.cs
@@ -0,0 +1,21 @@
+﻿using MobileView.Core;
+using MobileView.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileView.Winforms.Utilities
+{
+    public class SettingsManager
+    {
+        public string UserAgent { get => Properties.Settings.Default.UserAgent; set => Properties.Settings.Default.UserAgent = value; }
+        public SettingsManager() { }
+
+        public void Save()
+        { 
+            Properties.Settings.Default.Save();
+        }
+    }
+}
