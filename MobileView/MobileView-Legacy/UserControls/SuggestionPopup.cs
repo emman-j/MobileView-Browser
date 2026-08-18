ecc1a9c6bbcd554eb2cc03432ca1152b58fd2d8b
emman-j <176351546+emman-j@users.noreply.github.com>
2026-08-18 16:14:12 +0800

    legacy feat: add suggestion popup form for addressbar

---
+++ MobileView/MobileView-Legacy/UserControls/SuggestionPopup.cs
@@ -0,0 +1,27 @@
+﻿namespace MobileView.UserControls;
+internal class SuggestionPopup : Form
+{
+    public ListBox ListBox { get; } = new ListBox();
+
+    public SuggestionPopup()
+    {
+        FormBorderStyle = FormBorderStyle.None;
+        ShowInTaskbar = false;
+        StartPosition = FormStartPosition.Manual;
+        TopMost = true;
+
+        ListBox.Dock = DockStyle.Fill;
+        ListBox.BackColor = Color.FromArgb(39, 39, 39);
+        ListBox.ForeColor = Color.White;
+        ListBox.ItemHeight = 15;
+        Controls.Add(ListBox);
+    }
+
+    protected override bool ShowWithoutActivation => true;
+
+    private const int WS_EX_NOACTIVATE = 0x08000000;
+    protected override CreateParams CreateParams
+    {
+        get { var cp = base.CreateParams; cp.ExStyle |= WS_EX_NOACTIVATE; return cp; }
+    }
+}
